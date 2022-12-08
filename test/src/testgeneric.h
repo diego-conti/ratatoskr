e08686f2654e4ddf87cf70c43ea9517fbb870989
Diego Conti <diego.conti@unimib.it>
2022-12-08 18:25:18 +0100

    added email address

--- test/src/testgeneric.h
+++ test/src/testgeneric.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (C) 2022 by Diego Conti 
+ *  Copyright (C) 2022 by Diego Conti diego.conti@unipi.it 
  *  This file is part of Ratatoskr.                          
  *  Ratatoskr is free software; you can redistribute it and/or modify         
  *  it under the terms of the GNU General Public License as published by  
